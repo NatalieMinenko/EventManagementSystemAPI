92cb6fd68442bbe7122a26397a66d3d7485b9ac8
N_Minenko <natalie-0909@mail.ru>
2024-12-02 21:49:17 +0300

    AddDtosToCore

---
+++ EventManagementSystem.Core/DTOs/EventDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EventManagementSystem.DAL.DTOs;
+
+public class EventDto
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateTime { get; set; }
+    public string AdressOfEvent { get; set; }
+    public int MaxParticipants { get; set; }
+    public List<RegistrationDto> Registrations { get; set; }
+    public List<UserDto> Users { get; set; }
+}
