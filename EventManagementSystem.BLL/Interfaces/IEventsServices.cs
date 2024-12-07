6b1143f0435810d17ac2a69e85b0a50e558a65fe
N_Minenko <natalie-0909@mail.ru>
2024-12-07 23:21:13 +0300

    AddCorrectionsInEventsServices

--- EventManagementSystem.BLL/Interfaces/IEventsServices.cs
+++ EventManagementSystem.BLL/Interfaces/IEventsServices.cs
@@ -1,15 +1,10 @@
 ﻿using EventManagementSystem.BLL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventManagementSystem.BLL.Interfaces;
 
 public interface IEventsServices
 {
-    void AddEvent(EventModel events);
+    void AddEvent(CreateEventModel events);
     List<EventModel> GetAllEvents();
     EventModel GetEventById(Guid eventId);
     void UpdateEvent(Guid id, UpdateEventModel events);
