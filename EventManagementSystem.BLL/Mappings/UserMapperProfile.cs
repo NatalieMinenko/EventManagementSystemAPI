17714f7d7bf0d5938485f20537db2dfff3c3f2a8
N_Minenko <natalie-0909@mail.ru>
2024-12-07 22:00:52 +0300

    AddUsersServicesAndIUsersServices

---
+++ EventManagementSystem.BLL/Mappings/UserMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using EventManagementSystem.BLL.Models;
+using EventManagementSystem.DAL.DTOs;
+
+namespace EventManagementSystem.BLL.Mappings;
+public class UserMapperProfile : Profile
+{
+    public UserMapperProfile()
+    {
+        CreateMap<UserModel, UserDto>();
+        CreateMap<UserDto, UserModel>();
+    }
+}
---
+++ EventManagementSystem.Core/OptionsAuth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Core;
+
+public static class OptionsAuth
+{
+    public const string Issuer = "https://localhost:5001";
+    public const string Audience = "https://localhost:5001";
+    public const string Key = "superSecretKey@345superSecretKey@345QqqWww";
+}
